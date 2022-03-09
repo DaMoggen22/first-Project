5c318d5253f545069b1c9ad6f9c09336d143cae8
3TP <damoggen22@wp.pl>
2022-03-09 05:37:56 -0800

    metody splice

---
+++ js/SpliceMethods.js
@@ -0,0 +1,14 @@
+// let MyFish = ["anioł","klaun","mandarynka","chirurg"]
+// document.writeln("My Fish: "+MyFish)
+//
+// removed = MyFish.splice(2,0,"bęben")
+// document.write("<BR>Po dodaniu: "+MyFish+"<BR>")
+// document.write("Usunięto: "+removed)
+
+
+let MyDogs = ["Maja","Pusia","Tosia","Mandarynka"]
+document.writeln("Moje psy: "+MyDogs)
+let a = 2
+removed = MyDogs.splice(1,a,"Skalar","Gupik","Neonka","Bojownik")
+document.write("<BR>Po zmianach: "+MyDogs+"<BR>")
+document.write("Usunięto: "+removed)
